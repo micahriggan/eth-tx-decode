81809e15cc0968557e55427b1004e12242ae5dba
Micah Riggan <mriggan@bitpay.com>
2019-01-02 10:10:22 -0500

    Adding ERC20 decode

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const util = require('util');
+const AbiDecoder = require('abi-decoder');
 const EthereumTx = require('ethereumjs-tx');
+const erc20 = require('./erc20.json');
 const raw = '' 
 const tx = new EthereumTx(raw)
 console.log('FROM: 0x' + tx.from.toString('hex'));
@@ -10,3 +13,14 @@ console.log('VALUE: ', value);
 console.log('GAS PRICE: ', gasPrice);
 console.log('GAS TOTAL: ', gasLimit);
 console.log('ETH REQUIRED: ', value + gasPrice*gasLimit*10**-18 )
+
+
+const data = tx.data.toString('hex');
+if(data) {
+  console.log('============================');
+  console.log('======ERC20 DECODE==========');
+  AbiDecoder.addABI(erc20);
+  const decodedData = AbiDecoder.decodeMethod('0x' + data);
+  console.log(decodedData);
+}
+
